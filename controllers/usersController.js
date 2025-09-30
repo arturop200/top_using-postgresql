78f70b6784b41c1b10d3d847b2cebf3ba5eab260
Arturo Prado <67555134+arturop200@users.noreply.github.com>
2025-09-30 05:28:08 -0500

    feat(search): Implement case-insensitive username search

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,14 @@
 import * as db from '../db/queries.js';
 
 async function getUsernames(req, res) {
-  const usernames = await db.getAllUsernames();
+  const { search } = req.query;
+  let usernames;
+
+  if (search) {
+    usernames = await db.searchUsername(search.trim());
+  } else {
+    usernames = await db.getAllUsernames();
+  }
   console.log(`Usernames: ${usernames}`);
 
   res.render('pages/index', { title: 'Usernames', usernames: usernames });
--- db/queries.js
+++ db/queries.js
@@ -10,4 +10,14 @@ async function insertUsername(username) {
   await pool.query('INSERT INTO usernames (username) VALUES ($1)', [username]);
 }
 
-export { getAllUsernames, insertUsername };
+async function searchUsername(search) {
+  const searchPattern = `%${search}%`;
+  const { rows } = await pool.query(
+    'SELECT * FROM usernames WHERE username ILIKE $1',
+    [searchPattern],
+  );
+
+  return rows;
+}
+
+export { getAllUsernames, insertUsername, searchUsername };
